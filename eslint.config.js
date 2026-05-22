032d9ed74b547327ec5498f6f23cc36d3f7fbeaa
Florian <flox1an@localhost>
2026-05-22 12:45:47 +0200

    Add ESLint flat config

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+
+export default [
+  {
+    ignores: ['dist/**'],
+  },
+  ...tseslint.configs['flat/recommended'],
+  {
+    files: ['src/**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react-refresh/only-export-components': 'warn',
+    },
+  },
+];
